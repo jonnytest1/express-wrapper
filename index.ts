343d6f1838b7b9602fc0449cc0ca8e1a10114caf
jonny <ga86lad@mytum.de>
2021-04-27 20:39:38 +0200

    added tsocnfig/package json props

--- index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./src/express-hibernate-wrapper"
-export * from "./src/express-wrapper"
-export * from "./src/response-code-error"
-export * from "./src/util/settable"
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from "./express-hibernate-wrapper"
+export * from "./express-wrapper"
+export * from "./response-code-error"
+export * from "./util/settable"
