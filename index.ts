a06abe1d4bd3a234704028b00d2e4b2aa0e43785
jonny <ga86lad@mytum.de>
2021-04-27 20:29:19 +0200

    added hibernate setup

---
+++ index.ts
@@ -0,0 +1,4 @@
+export * from "./src/express-hibernate-wrapper"
+export * from "./src/express-wrapper"
+export * from "./src/response-code-error"
+export * from "./src/util/settable"
--- src/express-ws-type.d.ts
+++ src/express-ws-type.d.ts
@@ -1,7 +1,6 @@
 import { Express, Request } from 'express';
 export interface ExpressWs extends Express {
     ws(path, cb: (ws: Websocket, req: Request) => void)
-
 }
 
 export interface Websocket {
