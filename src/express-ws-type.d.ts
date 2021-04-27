e773a2dfda8450a97452eb6bbaa9cab23bc381ca
jonny <ga86lad@mytum.de>
2021-04-27 20:12:08 +0200

    initial files commit

---
+++ src/express-ws-type.d.ts
@@ -0,0 +1,13 @@
+import { Express, Request } from 'express';
+export interface ExpressWs extends Express {
+    ws(path, cb: (ws: Websocket, req: Request) => void)
+
+}
+
+export interface Websocket {
+    on: (type: string, cb: (data) => void) => void
+
+    send(data: string)
+
+    close()
+}
