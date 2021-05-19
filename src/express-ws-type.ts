bbb5af1bca9423038ac05699b13e177daf0a172e
jonny <ga86lad@mytum.de>
2021-05-19 12:28:28 +0200

    improved websocket type

--- src/express-ws-type.ts
+++ src/express-ws-type.ts
@@ -3,12 +3,20 @@ export interface ExpressWs extends Express {
     ws(path, cb: (ws: Websocket, req: Request) => void)
 }
 
+enum ReadyState { OPEN, CLOSED }
 export interface Websocket {
     on: (type: string, cb: (data) => void) => void
 
     send(data: string)
 
     close()
+
+
+    readyState: ReadyState
+
+    OPEN: ReadyState.OPEN
+
+    CLOSED: ReadyState.CLOSED
 }
 
 export const exportThisFilePls = ""
