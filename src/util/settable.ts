50af2855eb17b02c2ce1e0e89991e37fc6192a46
jonny <jonathanheindl@gmx.de>
2022-02-09 16:02:16 +0100

    settable can be configured to not overwrite

--- src/util/settable.ts
+++ src/util/settable.ts
@@ -1,10 +1,15 @@
 
-interface Setter {
+
+interface SetterConfig {
+    onlyWhenFalsy?: boolean
+}
+
+interface Setter extends SetterConfig {
     key: string;
     validation?(value): Promise<string>;
 }
 
-const setters: { [key: string]: Array<{ key: string, validation?: (value) => Promise<string> }> } = {}
+const setters: { [key: string]: Array<Setter> } = {}
 
 export function settableValidator(validationFunction) {
     return (target, propertyKey: string) => {
@@ -15,6 +20,15 @@ export function settableValidator(validationFunction) {
         setters[target.constructor].push({ key: propertyKey, validation: validationFunction });
     };
 }
+export function settableCfg(cfg: SetterConfig = {}) {
+    return function (target, propertyKey: string) {
+        //const objR = target as { __setters?: Array<Setter> };
+        if (!setters[target.constructor]) {
+            setters[target.constructor] = [];
+        }
+        setters[target.constructor].push({ key: propertyKey, ...cfg });
+    }
+}
 
 export function settable(target, propertyKey: string) {
     //const objR = target as { __setters?: Array<Setter> };
@@ -24,21 +38,30 @@ export function settable(target, propertyKey: string) {
     setters[target.constructor].push({ key: propertyKey });
 }
 
-export async function assign(obj, data) {
+export async function assign(obj, data, options: { onlyWhenFalsy?: boolean } = {}) {
     const objR = obj as { __setters?: Array<Setter> };
     const errorCollector = {};
     if (setters[obj.constructor]) {
         for (const key of setters[obj.constructor]) {
             if (Object.keys(data).map(k => k.split(".")[0]).includes(key.key)) {
+                let newObject;
                 if (key.validation) {
                     const errorObj = await key.validation.bind(objR)(data[key.key]);
                     if (errorObj) {
                         errorCollector[key.key] = errorObj;
+                        continue
                     } else {
-                        objR[key.key] = data[key.key];
+                        newObject = data[key.key];
                     }
                 } else {
-                    objR[key.key] = data[key.key];
+                    newObject = data[key.key];
+                }
+
+                if (newObject) {
+                    const onlyWhenFalsy = (options.onlyWhenFalsy || key.onlyWhenFalsy)
+                    if (!onlyWhenFalsy || !objR[key.key]) {
+                        objR[key.key] = newObject;
+                    }
                 }
             }
         }
